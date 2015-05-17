cf9ef0aacd67df8c05499bcea9b69225e64a568f
Vitaliy <dvwhitem@gmail.com>
2015-05-17 12:53:51 +0300

    replace ehcache.xml to CacheConfig annotation

--- src/main/java/com/timesheet/config/CacheConfig.java
+++ src/main/java/com/timesheet/config/CacheConfig.java
@@ -1,34 +1,61 @@
 package com.timesheet.config;
 
-import org.springframework.cache.CacheManager;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.config.CacheConfiguration;
+import org.springframework.cache.annotation.CachingConfigurer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.ehcache.EhCacheCacheManager;
-import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
+import org.springframework.cache.interceptor.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
 /**
  * Created by vitaliy on 14.05.15.
  */
 @Configuration
 @EnableCaching
-public class CacheConfig {
+public class CacheConfig implements CachingConfigurer {
 
-    @Bean
-    public EhCacheManagerFactoryBean ehCacheManagerFactoryBean() {
-        EhCacheManagerFactoryBean ehCacheManagerFactoryBean =
-                new EhCacheManagerFactoryBean();
+    final static String CACHE_POLICY = "LRU";
 
-        ehCacheManagerFactoryBean.setConfigLocation(new ClassPathResource("ehcache.xml"));
-        ehCacheManagerFactoryBean.setShared(true);
-        return ehCacheManagerFactoryBean;
+    @Bean(destroyMethod = "shutdown")
+    public CacheManager ehCacheManager() {
+        net.sf.ehcache.config.Configuration config = new net.sf.ehcache.config.Configuration();
+        config.addCache(cacheConfig("employeeList"));
+        return CacheManager.create(config);
     }
 
     @Bean
-    public CacheManager cacheManager() {
-        EhCacheCacheManager cacheManager = new EhCacheCacheManager();
-        cacheManager.setCacheManager(ehCacheManagerFactoryBean().getObject());
-        return cacheManager;
+    public org.springframework.cache.CacheManager cacheManager() {
+        return new EhCacheCacheManager(ehCacheManager());
     }
+
+
+    @Bean
+    public CacheResolver cacheResolver() {
+        return new SimpleCacheResolver(cacheManager());
+    }
+
+    @Bean
+    public KeyGenerator keyGenerator() {
+        return new SimpleKeyGenerator();
+    }
+
+    @Bean
+    public CacheErrorHandler errorHandler() {
+        return new SimpleCacheErrorHandler();
+    }
+
+    private CacheConfiguration cacheConfig(String name) {
+        CacheConfiguration config = new CacheConfiguration();
+        config.setName(name);
+        config.setMaxElementsInMemory(16384);
+        config.eternal(false);
+        config.timeToIdleSeconds(300);
+        config.timeToLiveSeconds(1200);
+        config.overflowToDisk(false);
+        config.setMemoryStoreEvictionPolicy(CACHE_POLICY);
+        return config;
+    }
+
 }
